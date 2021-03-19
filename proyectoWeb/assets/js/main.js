b4836c8e6456addc3099f0111f401dc97d6f9821
arielinarte <78271001+arielinarte@users.noreply.github.com>
2021-03-18 22:06:23 -0600

    Delete proyectoWeb directory

--- proyectoWeb/assets/js/main.js
+++
@@ -1,6 +0,0 @@
-let click = document.getElementById('menu-click');
-let enlaces = document.querySelector('.enlaces-header');
-
-click.addEventListener('click', () => {
-    enlaces.classList.toggle('click');
-});
