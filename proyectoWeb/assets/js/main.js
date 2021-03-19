a0d8dbe4d9998ed62e4ead4693b1730d98dd9a8b
arielinarte <78271001+arielinarte@users.noreply.github.com>
2021-03-18 21:50:32 -0600

    Add files via upload

---
+++ proyectoWeb/assets/js/main.js
@@ -0,0 +1,6 @@
+let click = document.getElementById('menu-click');
+let enlaces = document.querySelector('.enlaces-header');
+
+click.addEventListener('click', () => {
+    enlaces.classList.toggle('click');
+});
